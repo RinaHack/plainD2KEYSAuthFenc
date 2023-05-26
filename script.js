10a19bac7aaf7ca0ad4492d20807c44bcbc0ffad
RinaHack <133530822+RinaHack@users.noreply.github.com>
2023-05-26 06:23:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const rinauthk = {
     "D2": "f4a9d89a-0898-45a4-8e51-1427ed6bd27a",
     "k": {
         "f4a9d89a-0198-45a4-8e51-1427ed6bd27a": false,
-        "f4a9d89a-0298-45a4-8e51-1427ed6bd27a": false,
-        "f4a9d89a-0398-45a4-8e51-1427ed6bd27a": true,
-        "f4a9d89a-0498-45a4-8e51-1427ed6bd27a": true,
-        "f4a9d89a-0598-45a4-8e51-1427ed6bd27a": true,
-        "f4a9d89a-0698-45a4-8e51-1427ed6bd27a": true,
-        "f4a9d89a-0798-45a4-8e51-1427ed6bd27a": true,
-        "f4a9d89a-0898-45a4-8e51-1427ed6bd27a": true,
-        "f4a9d89a-0998-45a4-8e51-1427ed6bd27a": true
+        "gqy4mNPV0pgRube": false,
+        "fa4-mbn-95-fa9d89a": false,
+        "fd89a-0498-45a4-8e51-1427ed6bd27a": false,
+        "f4a9d89a-8-45a4-8e51-1427ed6bd27a": false,
+        "f4a9d89a-069a": false,
+        "Trap2808": true,
+        "xR3wwrsADGtRqWa2GdE6h": false,
+        "f4a9d89a-0998-45a4-8e51-d27a": false
     },
     "init": () => {console.log('kservice initialized');}
 };
