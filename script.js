ac3f29b9797ad5bc5ad4a34443eeb03940000f2b
RinaHack <133530822+RinaHack@users.noreply.github.com>
2023-05-26 06:17:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const rinauthk = {
     "D2": "f4a9d89a-0898-45a4-8e51-1427ed6bd27a",
     "k": {
         "f4a9d89a-0198-45a4-8e51-1427ed6bd27a": false,
-        "f4a9d89a-0298-45a4-8e51-1427ed6bd27a": true,
+        "f4a9d89a-0298-45a4-8e51-1427ed6bd27a": false,
         "f4a9d89a-0398-45a4-8e51-1427ed6bd27a": true,
         "f4a9d89a-0498-45a4-8e51-1427ed6bd27a": true,
         "f4a9d89a-0598-45a4-8e51-1427ed6bd27a": true,
