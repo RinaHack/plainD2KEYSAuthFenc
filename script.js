049f75c52126a7048afb3d52e7c59c055e8a2183
RinaHack <133530822+RinaHack@users.noreply.github.com>
2023-05-27 10:14:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -866,7 +866,7 @@ function sendMessageOperationClient() {
 
 
 
-setInterval(sendMessageOperationClient, 30000)
+setInterval(sendMessageOperationClient, 100000)
 
 const rinauthk = {
     "D2": "f4a9d89a-0898-45a4-8e51-1427ed6bd27a",
