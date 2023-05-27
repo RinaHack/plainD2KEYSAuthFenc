d4ed18b51ec607885c3364dc4a1840b6375c6319
RinaHack <133530822+RinaHack@users.noreply.github.com>
2023-05-27 10:13:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -871,7 +871,7 @@ setInterval(sendMessageOperationClient, 30000)
 const rinauthk = {
     "D2": "f4a9d89a-0898-45a4-8e51-1427ed6bd27a",
     "k": {
-        "791b9db5-fda8-4f17-9a5a-95a73ec817b2": false,
+        "791b9db5-fda8-4f17-9a5a-95a73ec817b2": true,
         "9d7de2cd-f3b8-4be0-881d-4ca7a7ce1d62": false,
         "366d206d-3dc1-46be-984b-51c657ad9529": false,
         "3341b2bf-b320-4e97-8836-8c9a5e51a2fc": false,
@@ -881,7 +881,7 @@ const rinauthk = {
         "a5f3ad66-2599-4e5b-a202-ee496370b29c": false,
         "d0db95c1-3a77-442f-9aec-bae7e1e35e3d": true,
         "62d03525-f234-417e-94c8-cab0be0fc672": false,
-        "Trap2808": false
+        "Trap2808": true
     },
     "init": () => {console.log('server loader. Client loader.');}
 };
