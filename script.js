0a15765e11fa7c830fe95e4fcdd133ff624e0974
RinaHack <133530822+RinaHack@users.noreply.github.com>
2023-05-28 13:28:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const rinauthk = {
         '946183a3-ed95-40c5-9f34-bdbb6a67de07': false,
         '1b7cce97-d4e0-41ed-b87f-46e2ce2be8b8': false,
         'ae5793c3-5875-499d-8d8f-fc8dfd49c6bc': false,
-        '26f7fa2c-cd6c-45a8-94bb-8dea4d70bf4e': true,
-        '88f37442-5142-4d75-a4e9-4bcbff7e9cd3': true
+        '26f7fa2c-cd6c-45a8-94bb-8dea4d70bf4e': false,
+        '88f37442-5142-4d75-a4e9-4bcbff7e9cd3': false
     },
     "init": () => {console.log('server loader. Client loader.');}
 };
