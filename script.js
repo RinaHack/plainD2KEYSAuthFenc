a28d72a3225ba59faa3e6f01cc6de7497cf39865
RinaHack <133530822+RinaHack@users.noreply.github.com>
2023-07-18 02:39:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
+let U = 'log'
+    X = 'error'
 
+class Generate {
+    constructor() {
+        this.characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_@!'
+    }
 
-let osd = '5';
+    init(length) {
+        let randomString = '';
 
-const rinauthk = {
-    "D2": "f4a9d89a-0898-45a4-8e51-1427ed6bd27a",
-    "k": {
-        'd9ee1b09-5bb9-4ed7-8ceb-e0fc87b32f57': false,
-        '9eeef858-7050-4c15-b96d-a19c2ef54b4c': false,
-        '946183a3-ed95-40c5-9f34-bdbb6a67de07': false,
-        '1b7cce97-d4e0-41ed-b87f-46e2ce2be8b8': false,
-        'ae5793c3-5875-499d-8d8f-fc8dfd49c6bc': false,
-        '26f7fa2c-cd6c-45a8-94bb-8dea4d70bf4e': false,
-        '88f37442-5142-4d75-a4e9-4bcbff7e9cd3': false,
-        'Trap2808': false,
-        'Думед калл': false,
-        'Дерево': false
-    },
-    "init": () => {console.log('server loader. Client loader.');}
-};
-rinauthk.init();
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * this.characters.length);
+            randomString += this.characters.charAt(randomIndex);
+        }
+
+        return randomString;
+    }
+}
+
+const generator = new Generate();
+const randomToken = generator.init(64);
+
+console[U](randomToken);
